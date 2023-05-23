ade1f05ac09adff14b47e57af93e9fc18de1f566
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-05-23 18:09:48 -0500

    First commit

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,19 @@
+package org.example.application;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+        launch();
+
+    }
+
+
+
+    public void start(PrimaryStage stage){
+
+
+
+
+
+    }
+}
