c017f87d9be3d084410f37258ea75e2382cb76bd
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-05-30 12:40:04 -0500

    Bug fixed

--- src/main/java/org/project/utilities/userTimes.sh
+++ src/main/java/org/project/utilities/userTimes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-last --since "3 month ago"| grep -v logout | grep seat0 | awk '{print $1,$NF}' | sed 's/[()]//g' | python3 UsTimeCalc.py
+last --since "3 month ago"| grep -v logout | grep ":0" | awk '{print $1,$NF}' | sed 's/[()]//g' | python3 UsTimeCalc.py
 
